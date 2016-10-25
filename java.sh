abf058857d903b16eba47aed7e2020a9afece8ab
larkvincer <pku7iv@gmail.com>
2016-10-25 19:28:12 +0300

    java.sh final version

--- java.sh
+++ java.sh
@@ -1,56 +1,30 @@
 #!/bin/bash
 
+echo ">>Java installation started<<"
 
-#Only user with uid=0 have root privileges
-ROOT_UID=0
+# Include buch of helpful functions
+source ./core/main_functions.sh
+
+# Only root can run this script
+check_root
 
 #folder where java will be installed
-DEST="/opt/"
-#name of folder where java will be installed
-FOLDER="java"
+readonly DWN_FOLDER="/opt"
 #link for java source
-BASE_URL="http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz"
-#Source file name
-SRC=`echo ${BASE_URL} | rev | cut -d "/" -f 1 | rev`
+readonly DWN_URL="http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.tar.gz"
+readonly DWN_PACKAGE="jdk.tar.gz"
+# File for with commands to set environment variables
+readonly PATH_FILE="$HOME/Desktop/java_installation.txt"
 
+#Downloading package
+download_package "$DWN_URL" "$DWN_PACKAGE" "$DWN_FOLDER" "Cookie: oraclelicense=accept-securebackup-cookie"
+#wget -c --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "${BASE_URL}"
 
-#Make sure script runs by root user
-if [ "$UID" -ne "$ROOT_UID" ]
-then
-    echo "This script must be run as root"2>&1
-    exit $E_NOTROOT
-fi
+# Extract archive
+unpack_package "$DWN_PACKAGE" "$DWN_FOLDER"
 
-echo
-echo "**********************************
-INSTALLING JAVA (version = `sed -i 's/u/./g' < echo ${BASE_URL} | rev | cut -d '/' -f 1 | rev | cut -d '-' -f 2`)
-**********************************"
-echo
-exit 1;
+# File for setting path
+echo "export JAVA_HOME=/opt/$DIR_NAME" >> "$PATH_FILE"
+echo "export PATH=$PATH:$JAVA_HOME" >> "$PATH_FILE"
 
-#Move to installation folder
-cd ${DEST}
-mkdir -p ${FOLDER}
-cd ${FOLDER}
-
-#Download java source
-echo
-echo "DOWNLOAD JAVA SOURCE FROM"
-wget -c --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "${BASE_URL}"
-
-#Extracting java from archive
-#gunzip ${SRC}
-#Get extracted folder name
-echo ${SRC}
-DIR_NAME=`tar -tzf ${SRC} | head -1 | cut -f 1 -d "/"`
-
-#extract archive
-echo
-echo "EXTRACTING JAVA FROM ARCHIVE"
-tar -xvzf ${SRC}
-#Delete archive
-echo "DELETING JAVA ARCHIVE"
-
-echo "JAVA WAS SECCESSFUlY INSTALLED IN ${DEST}${FOLDER}"
-
-exit 0;
+echo ">>Java was seccessfuly installed<<"
