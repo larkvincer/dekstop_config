98c670fd25ecc7520d3cb288c82c5c6120ab6022
larkvincer <pku7iv@gmail.com>
2016-10-25 19:07:03 +0300

    sublime script added

---
+++ sublime.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo ">>Sublime text editor installation<<"
+
+# Include bash script with custom functions
+source ./core/main_functions.sh
+
+# Check wether script run under root
+check_root
+
+readonly DWN_URL="https://download.sublimetext.com/sublime-text_build-3126_amd64.deb"
+readonly DWN_FOLDER="$HOME/Desktop"
+readonly PACKAGE_NAME="sublime.deb"
+
+# Download archive
+download_package "$DWN_URL" "$PACKAGE_NAME" "$DWN_FOLDER"
+
+# Install dpkg package
+dpkg -i "$PACKAGE_NAME"
+
+# Remove deb package
+rm -f "$PACKAGE_NAME"
+
+# Download and install package manager for sublime text editor
+download_package "https://packagecontrol.io/Package%20Control.sublime-package" "Package Control.sublime-package" "$HOME/.config/sublime-text-3"
+
+echo ">>Sublime installation completed<<"
