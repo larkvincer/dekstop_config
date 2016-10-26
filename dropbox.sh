911b7508ac69ad37bc787966a8291f821108d80d
larkvincer <larkvincer@gmail.com>
2016-10-26 18:32:12 +0300

    dropbox script added

---
+++ dropbox.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo
+echo ">>dropbox installation started<<"
+
+# Include buch of helpful functions
+source ./core/main_functions.sh
+
+# Only root can run this script
+check_root
+
+readonly DWN_URL="https://www.dropbox.com/download?plat=lnx.x86_64"
+readonly DWN_FOLDER="$HOME"
+readonly DWN_PACKAGE="db.tar.gz"
+
+#Downloading package
+#download_package "$DWN_URL" "$DWN_PACKAGE" "$DWN_FOLDER"
+
+# Extract archive
+unpack_package "$DWN_PACKAGE" "$DWN_FOLDER"
+
+echo $DIR_NAME
+cd $DIR_NAME
+./dropboxd
+
+
+echo ">>dropbox was seccessfuly installed<<"
+echo
